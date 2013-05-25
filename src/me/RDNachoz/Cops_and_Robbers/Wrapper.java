85f2b057befe50ae79b7269c8010bc896e6a53f2
Steffion <stefdegoey@gmail.com>
2013-05-25 20:42:51 +0200

    Added Wrapper.

---
+++ src/me/RDNachoz/Cops_and_Robbers/Wrapper.java
@@ -0,0 +1,8 @@
+package me.RDNachoz.Cops_and_Robbers;
+
+import me.RDNachoz.Cops_and_Robbers.managers.MessageManager;
+
+public class Wrapper {
+	public static CR instance;
+	public static MessageManager mm = new MessageManager("b", "c", "4", "e", "9");
+}
